5aa280ec6c8dae634df3dcf26bd9faae4596bec0
amomatter <amomatter48@gmail.com>
2023-07-03 21:23:14 +0300

    first commit

---
+++ app/Console/Kernel.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console;
+
+use App\Console\Commands\expiration;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+
+
+    protected $commands = [
+
+        expiration::class,
+
+    ];
+
+
+    protected function schedule(Schedule $schedule)
+    {
+         $schedule->command('user:expire')->everyMinute();
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
